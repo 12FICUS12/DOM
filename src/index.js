d588c06065b997d55f2965fdfaa9ea954b17adfc
Maria <maria.Alekseeva14@mail.ru>
2025-02-15 17:53:12 +0300

    problem

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './css/style.css';
+console.log('Main script is loaded');
 import './js/goblin.js'
+console.log('gob script is loaded');
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,65 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 
 module.exports = {
     mode: 'production',
-    entry: './src/index.js', // Входная точка
-    output: {
-        path: path.resolve(__dirname, 'dist'), // Выходная директория
-        filename: 'bundle.js',  // Имя выходного файла
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // Для JavaScript файлов
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.css$/, // Для CSS файлов
-                use: ['style-loader', 'css-loader'], // Лоадеры для обработки CSS
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i, // Обработка изображений
-                type: 'asset/resource',
-            },
+    entry: './src/index.js',  // укажите точку входа
+     output: {
+       path: path.resolve(__dirname, 'dist'),
+       filename: 'main.js',    // укажите имя для выходного JavaScript
+       clean: true,            // очищаем выходную папку перед сборкой
+     },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html', // Шаблон HTML файла
-        }),
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
     ],
-    stats: {
-        children: true, // Включает статистику компилируемых детей
-    },
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
